d4ad67d369fd280bbf5c7dabbe7e731cab8d39d6
Aditya-13 <59966722+Aditya-13@users.noreply.github.com>
2021-07-08 12:38:56 +0530

    Update notepad.py

--- notepad.py
+++ notepad.py
@@ -1,5 +1,6 @@
 from tkinter import *
-import os
+from tkinter import filedialog
+import time
 
 window = Tk()
 window.title('Notepad')
@@ -9,39 +10,37 @@ window.geometry("%dx%d" % (width, height))
 
 my_menu = Menu(window)
 window.config(menu=my_menu)
+global_filename = ''
+def open_file():
+    filename = filedialog.askopenfilename(initialdir = "/", title = "Select a File", filetypes = (("Text files", "*.txt*"),("all files", "*.*")))
+    return filename
 
 def our_command():
     my_label = Label(window, text="Clicked!!").pack()
 
-def get_saved_text():
-    def get_file_name(): 
-        window1 = Toplevel(window)
-        window1.title('Enter File Name ')
-        label = Label(window1, text='Enter File Name To Save:', font="Montserrat 10")
-        label.pack()
-        textArea1 = Entry(window1, bg="white", fg="black", font="Lato 12", )
-        textArea1.pack()
-        label1 = Label(window1, text=' ', font="Montserrat 10")
-        label1.pack()
-        save_button = Button(window1, text='Save', font="Lato 10", command=textArea1.get())
-        save_button.pack()
-        label2 = Label(window1, text=' ', font="Montserrat 10")
-        label2.pack()
-        exit_button = Button(window1, text='Exit', font="Lato 10", command=window1.destroy)
-        exit_button.pack()
-        window1.geometry('400x200')
-        return textArea1.get
-    get_file_name()
-    text = textArea.get("1.0",END)
-    print(get_file_name())
-    return text
+def get_opened_file_name():
+    global_filename = open_file()
+    return global_filename
 
+def save_opened_file():
+    text = textArea.get("1.0", END)
+    global_filename = get_opened_file_name()
+    f = open(global_filename, "w")
+    f.write('')
+    time.sleep(1)
+    f.write(text)
+
+def open_edit_file():
+    filename = get_opened_file_name()
+    with open(file=filename) as file:
+        data = file.read()        
+        textArea.insert(END, data)    
 
 file_menu= Menu(my_menu)
 my_menu.add_cascade(label="File", menu=file_menu)
 file_menu.add_command(label="New",command=our_command)
-file_menu.add_command(label="Open",command=our_command)
-file_menu.add_command(label="Save",command=get_saved_text)
+file_menu.add_command(label="Open",command=open_edit_file)
+file_menu.add_command(label="Save",command=save_opened_file)
 
 edit_menu = Menu(my_menu)
 my_menu.add_cascade(label="Edit",menu=edit_menu)
@@ -51,7 +50,6 @@ edit_menu.add_command(label="Copy",command=our_command)
 option_menu = Menu(my_menu)
 my_menu.add_cascade(label="Options",menu=option_menu)
 option_menu.add_command(label="Exit",command=window.quit)
-
 
 textArea = Text(window, width=width, bg="white", fg="black", font="Lato 14",)
 textArea.pack()
